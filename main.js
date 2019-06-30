8cb5d8439671be4f08cc82be8cc2b0de8af9893d
gerritt <gmocore@gmail.com>
2019-06-30 12:59:55 -0600

    created simons color pick function  with console log

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// color[0].id 
 const color = document.querySelectorAll(".pad")
-
-console.log(color);
-
-
 const simonsChoice = []
 
 const colorPicker = function() {
     simonsChoice.push(Math.floor(Math.random() * color.length))
     console.log(simonsChoice);
+}
+
+const simonsColorPicks = () => {
+// TODO  chnage console log to a computer clicking
+    simonsChoice.forEach(item => console.log(color[item].id))
 }
