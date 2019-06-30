d4bcffd2e1fa996694aa2c196a0a09f929549756
gerritt <gmocore@gmail.com>
2019-06-30 16:39:48 -0600

    Basic functionality for play button

--- main.js
+++ main.js
@@ -1,37 +1,70 @@
+// Variables
 const color = document.querySelectorAll(".pad")
 const simonsChoice = []
+const userChoice = []
 const playButton = document.getElementById('play-button') 
 
+// eventListener
+color.forEach(item => {
+    item.addEventListener('click', () => {
+        console.log(this.event.target)
+        console.log(this.event)
+        // let clickedTarget = this.event.target
+        userChoice.push(this.event.target)
+        // return clickedTarget
+    })
+})
 
+playButton.addEventListener('click', () => {
+    playGame()
+})
 
+// functions
 const colorPicker = function() {
     simonsChoice.push(Math.floor(Math.random() * color.length))
     console.log(simonsChoice);
 }
 
 const simonsColorPicks = () => {
-// TODO  chnage console log to a computer clicking
     simonsChoice.forEach(item => console.log(color[item].id))
+    simonsChoice.forEach(item => color[item].style.filter = 'opacity(0.5)')
 }
 
-color.forEach(item => {
-    console.log(item.id)
-    item.addEventListener('click', () => {
-        console.log(this.event.target)
-    })
-})
+// const userColorPicks = () => {
+//     userChoice.push(clickedTarget)
+// }
 
-playButton.addEventListener('click', () => {
-    console.log(123);
-    
-})
+const playGame = () => {
+    colorPicker()  
+    simonsColorPicks()
+}
 
 
 
 
-// const blue = document.getElementById('blue')
-// blue.addEventListener("click", () => {
-//     console.log("clciked");
-    
-// })
+// crazy compare function
+function compare(userChoice,simonsChoice){
+  
+    if(!userChoice  || !simonsChoice) return
+   
+     let result;
+   
+   userChoice.forEach((e1,i)=>simonsChoice.forEach(e2=>{
+     
+          if(e1.length > 1 && e2.length){
+             result = compare(e1,e2);
+          }else if(e1 !== e2 ){
+             result = false
+          }else{
+             result = true
+          }
+     })
+   )
+   
+   return result
+   
+ }
+
+
+
 
