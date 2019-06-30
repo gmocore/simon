17f87ba153086cccda3527fc523fb3e5245426d7
gerritt <gmocore@gmail.com>
2019-06-30 13:38:04 -0600

    added play button and event listeners for each pad

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const color = document.querySelectorAll(".pad")
 const simonsChoice = []
+const playButton = document.getElementById('play-button') 
+
+
 
 const colorPicker = function() {
     simonsChoice.push(Math.floor(Math.random() * color.length))
@@ -10,3 +13,25 @@ const simonsColorPicks = () => {
 // TODO  chnage console log to a computer clicking
     simonsChoice.forEach(item => console.log(color[item].id))
 }
+
+color.forEach(item => {
+    console.log(item.id)
+    item.addEventListener('click', () => {
+        console.log(this.event.target)
+    })
+})
+
+playButton.addEventListener('click', () => {
+    console.log(123);
+    
+})
+
+
+
+
+// const blue = document.getElementById('blue')
+// blue.addEventListener("click", () => {
+//     console.log("clciked");
+    
+// })
+
